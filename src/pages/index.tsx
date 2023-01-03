3824ceef8c471455b0c53c5d39d75b572a18f9eb
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-03 10:03:02 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
+import { ArticlesList } from "src/features/article/components/ArticlesList";
 
 const Home = () => {
   return (
     <div className="p-4">
       <Header />
       <Hero />
+      <ArticlesList />
     </div>
   );
 };
