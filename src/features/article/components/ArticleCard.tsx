8609dddc35a95f93267562ffbd6dcb79a042cd55
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-03 10:02:09 +0100

    article card component

---
+++ src/features/article/components/ArticleCard.tsx
@@ -0,0 +1,28 @@
+import type { Article } from "@prisma/client";
+
+type ArticleCardProps = {
+  article: Article;
+};
+
+export const ArticleCard = ({ article }: ArticleCardProps) => {
+  return (
+    <article className="rounded-xl border ">
+      <img
+        src={article.imageUrl}
+        alt={article.title}
+        className="h-64 w-full rounded-t-xl bg-contain"
+      />
+      <div className="p-4">
+        <h3 className="text-xl font-bold text-gray-800">{article.title}</h3>
+        <p className="text-gray-600">{article.description}</p>
+        <div className="flex items-center gap-x-4">
+          <div className="h-10 w-10 rounded-full bg-gray-300"></div>
+          <div className="flex flex-col">
+            <span>Atan Ake</span>
+            <span>May 13, 2022</span>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
