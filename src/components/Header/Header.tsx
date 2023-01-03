d88953d7d267259cbd9e6f88f1a3baa5a3fdd643
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-03 11:59:00 +0100

    show user avatar when logged in

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,9 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { FcHome } from "react-icons/fc";
 
 export const Header = () => {
+  const { data: session, status } = useSession();
   return (
     <header className="mx-auto flex max-w-7xl items-center justify-between py-8">
       <span className="text-3xl font-bold text-gray-700">Blog App</span>
@@ -13,12 +15,24 @@ export const Header = () => {
           <FcHome className="text-2xl" />
           <span>Home</span>
         </Link>
-        <Link
-          href="/signin"
-          className="rounded-full bg-gray-400 px-6 py-2 font-bold text-white"
-        >
-          Sign In
-        </Link>
+
+        {status === "unauthenticated" && (
+          <Link
+            href="/signin"
+            className="rounded-full bg-gray-400 px-6 py-2 font-bold text-white"
+          >
+            Sign In
+          </Link>
+        )}
+        {status === "authenticated" && session.user && (
+          <button className="flex items-center gap-x-2">
+            <img
+              src={session.user.image ?? ""}
+              alt={session.user.name ?? "Avatar"}
+              className="h-12 w-12 rounded-full"
+            />
+          </button>
+        )}
       </nav>
     </header>
   );
