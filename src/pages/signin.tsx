1bf53040b4d610de5ec20a3c0745350739b40d05
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-03 12:29:02 +0100

    redirect on successful signin

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -8,7 +8,7 @@ const SignInPage = () => {
       <main className="mx-auto flex max-w-7xl justify-center">
         <div className="mt-24 flex w-full max-w-lg flex-col items-center rounded-lg border bg-gray-50 p-4 shadow-md">
           <button
-            onClick={() => signIn("github")}
+            onClick={() => signIn("github", { callbackUrl: "/" })}
             className="rounded-full bg-teal-500 px-6 py-2 font-bold text-white transition hover:scale-95 hover:opacity-75 active:scale-100"
           >
             Sign In with Github
