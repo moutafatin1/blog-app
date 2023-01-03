78e69ee6b222c18c95cb276617e81e909bf7bd14
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-03 09:13:41 +0100

    hero section prototype

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+export const Hero = () => {
+  return (
+    <section className="mx-auto max-w-7xl rounded-3xl bg-gradient-to-t from-[#647DEE] to-[#7F53AC] py-16 px-4 text-center">
+      <h1 className="text-4xl font-bold text-white md:text-5xl">
+        The Developer&apos;s Corner
+      </h1>
+      <p className="mt-4 text-lg text-gray-100">
+        A community for developers to share their knowledge and experiences
+      </p>
+    </section>
+  );
+};
