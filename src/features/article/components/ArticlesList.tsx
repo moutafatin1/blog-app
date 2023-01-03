a52a7188c2a2e78bc05ce03e2b150c69b0a50f04
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-03 11:59:10 +0100

    add better loading spinner

--- src/features/article/components/ArticlesList.tsx
+++ src/features/article/components/ArticlesList.tsx
@@ -1,16 +1,25 @@
+import { Spinner } from "@/components/Spinner";
 import { trpc } from "@/utils/trpc";
 import { ArticleCard } from "./ArticleCard";
 
 export const ArticlesList = () => {
   const { data: articles, isLoading, error } = trpc.article.all.useQuery();
 
-  if (isLoading) return <p>Loading....</p>;
   if (error) return <p>{error.message}</p>;
   return (
     <div className="mx-auto mt-16 grid max-w-7xl gap-4  sm:grid-cols-2 lg:grid-cols-3">
-      {articles.map((article) => (
-        <ArticleCard key={article.id} article={article} />
-      ))}
+      {isLoading ? (
+        <Spinner
+          delay={400}
+          show={isLoading}
+          className="col-span-3 mx-auto text-7xl text-purple-500"
+        />
+      ) : (
+        articles.map((article) => (
+          <ArticleCard key={article.id} article={article} />
+        ))
+      )}
+      {}
     </div>
   );
 };
