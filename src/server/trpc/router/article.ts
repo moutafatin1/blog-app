96e3a3d9cd8f3a65917e3df80808a8346c9aa22b
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-04 08:49:47 +0100

    get article by slug endpoint

--- src/server/trpc/router/article.ts
+++ src/server/trpc/router/article.ts
@@ -1,7 +1,15 @@
+import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 
 export const articleRouter = router({
   all: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.article.findMany();
+  }),
+  bySlug: publicProcedure.input(z.string()).query(({ ctx, input }) => {
+    return ctx.prisma.article.findUnique({
+      where: {
+        slug: input,
+      },
+    });
   }),
 });
