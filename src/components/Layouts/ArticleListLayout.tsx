c0214afcf6cf2fc912a5fccb3d3b01a16544c3ca
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-04 17:44:01 +0100

    remove border radius

--- src/components/Layouts/ArticleListLayout.tsx
+++ src/components/Layouts/ArticleListLayout.tsx
@@ -14,11 +14,9 @@ export const ArticleListLayout = ({ children }: ArticleListLayoutProps) => {
       <Header />
       <Hero />
       <div className="mx-auto mt-16 flex max-w-7xl items-start gap-4">
-        <div className="rounded-2xl border">
+        <div className="rounded-t-2xl border">
           <ViewArticlesBy />
-          <div className="mx-auto grid gap-4 rounded-2xl pt-4  sm:grid-cols-2">
-            {children}
-          </div>
+          <div className="mx-auto gap-4">{children}</div>
         </div>
         <Tags />
       </div>
