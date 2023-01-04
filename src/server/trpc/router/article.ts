a5685586d8f6131770f8631d8a02189ab8389e4f
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-04 11:38:13 +0100

    filter article by tag name endpoint

--- src/server/trpc/router/article.ts
+++ src/server/trpc/router/article.ts
@@ -2,9 +2,32 @@ import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 
 export const articleRouter = router({
-  all: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.article.findMany();
-  }),
+  all: publicProcedure
+    .input(
+      z
+        .object({
+          tagName: z.string().optional(),
+        })
+        .optional()
+    )
+    .query(({ ctx, input }) => {
+      return ctx.prisma.article.findMany({
+        where: {
+          tags: {
+            some: {
+              name: input?.tagName,
+            },
+          },
+        },
+        include: {
+          tags: {
+            where: {
+              name: input?.tagName,
+            },
+          },
+        },
+      });
+    }),
   bySlug: publicProcedure.input(z.string()).query(({ ctx, input }) => {
     return ctx.prisma.article.findUnique({
       where: {
