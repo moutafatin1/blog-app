69ad874b48ab1fbf95098b7769301ca235522ad3
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-04 08:49:13 +0100

    add link to the title

--- src/features/article/components/ArticleCard.tsx
+++ src/features/article/components/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import type { Article } from "@prisma/client";
+import Link from "next/link";
 
 type ArticleCardProps = {
   article: Article;
@@ -13,7 +14,12 @@ export const ArticleCard = ({ article }: ArticleCardProps) => {
         className="h-64 w-full rounded-t-xl bg-contain"
       />
       <div className="p-4">
-        <h3 className="text-xl font-bold text-gray-800">{article.title}</h3>
+        <Link
+          href={`/article/${article.slug}`}
+          className="text-xl font-bold text-gray-800"
+        >
+          {article.title}
+        </Link>
         <p className="text-gray-600">{article.description}</p>
         <div className="flex items-center gap-x-4">
           <div className="h-10 w-10 rounded-full bg-gray-300"></div>
